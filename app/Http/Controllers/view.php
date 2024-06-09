00f30a24f9c4e2b635acfc61fc5d59975bf8760c
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2024-06-09 12:37:49 +0700

    Push Code

--- app/Http/Controllers/view.php
+++ app/Http/Controllers/view.php
@@ -3,70 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Arr;
 use App\Models\Value;
 use App\Models\Pasien;
-use App\Models\sensor;
-use Response;
 
-class view extends Controller
+class View extends Controller
 {
-  
-    public function sensor($idAlat, $dropsPerMinutes ,$kapasitas, $status){
-        if($kapasitas<0){
+    public function sensor($idAlat, $dropsPerMinutes, $kapasitas, $status)
+    {
+        if ($kapasitas < 0) {
             $kapasitas = 0;
         }
-        $pasien = Pasien::where('alat', $idAlat)->where('status','1')->first();
-        $prediksi = ($kapasitas/$dropsPerMinutes)/4;
+        $pasien = Pasien::where('alat', $idAlat)->where('status', '1')->first();
+        $prediksi = ($kapasitas / $dropsPerMinutes) / 4;
         $dropsPerMinutes = $dropsPerMinutes - 1;
 
-        Value::Create([
-                
-            'idPasien'=> $pasien->id,
-            'tpm'=> $dropsPerMinutes,
-            'kapasitas'=> $kapasitas,
-            'prediksi'=> $prediksi,
+        Value::create([
+            'idPasien' => $pasien->id,
+            'tpm' => $dropsPerMinutes,
+            'kapasitas' => $kapasitas,
+            'prediksi' => $prediksi,
             'status' => $status
-            
         ]);
-
     }
 
     public function index()
     {
-       
-       $Tempdata = Pasien::with('sensor.Value')->get();
+        $tempData = Pasien::with('sensor.Value')->get();
+        $tempValue = Value::get();
 
-       $Tempvalue = Value::get();
-       
-       $data = $Tempdata->toArray();
-       $value = $Tempvalue->toArray();
-     
+        $data = $tempData->toArray();
+        $value = $tempValue->toArray();
 
-       if(count($Tempdata) == !0 ){
-        return view('index',['data'=>$data]);
-       }
-       else{
-        return view('index',['data'=>0]);
-       }
-      
-    }
-    public function get(){
-        $Tempdata = Pasien::with('sensor.Value')->get();
-       $Tempvalue = Value::get();
-       $data = $Tempdata->toArray();
-       $value = $Tempvalue->toArray();
-   
-       
-  
-       if(count($Tempdata) == !0 ){
-        return response()->json(['value'=>$value, 'data'=>$data], 200);
-       }
-       else{
-        dd("dsadsa");
-       }
+        return view('index', ['data' => count($tempData) ? $data : 0]);
     }
 
-   
+    public function get()
+    {
+        $tempData = Pasien::with('sensor.Value')->get();
+        $tempValue = Value::get();
+
+        $data = $tempData->toArray();
+        $value = $tempValue->toArray();
+
+        return count($tempData) ? response()->json(['value' => $value, 'data' => $data], 200) : dd("dsadsa");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Di sini Anda bisa mendaftarkan web routes untuk aplikasi Anda. Routes ini
+| dimuat oleh RouteServiceProvider dalam sebuah grup yang berisi middleware "web".
+| Sekarang buatlah sesuatu yang hebat!
 |
 */
 
@@ -18,13 +18,14 @@ Route::get('/get', [App\Http\Controllers\View::class, 'get'])->name('get');
 Auth::routes();
 Route::get('/sensor/{idAlat}/{dropsPerMinutes}/{kapasitas}/{status}', [App\Http\Controllers\View::class, 'sensor']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('auth')->name('home');
-Route::get('/regist', [App\Http\Controllers\regist::class, 'index'])->name('regist');
+Route::get('/regist', [App\Http\Controllers\Regist::class, 'index'])->name('regist');
 Route::post('/register', [App\Http\Controllers\Regist::class, 'create'])->name('register');
-Route::get('/adddevice', [App\Http\Controllers\addDeviceController::class, 'index'])->middleware('auth')->name('adddevice');
+Route::get('/adddevice', [App\Http\Controllers\AddDeviceController::class, 'index'])->middleware('auth')->name('adddevice');
 Route::get('updates', [App\Http\Controllers\UpdateController::class, 'index'])->middleware('auth')->name('updates');
 Route::post('update', [App\Http\Controllers\UpdateController::class, 'update']);
-Route::post('adddevices', [App\Http\Controllers\addDeviceController::class, 'post']);
-Route::get('/device', [App\Http\Controllers\addDeviceController::class, 'list'])->middleware('auth')->name('device');
-Route::get('deletedevice/{id}',[App\Http\Controllers\UpdateController::class, 'delete']);
+Route::post('adddevices', [App\Http\Controllers\AddDeviceController::class, 'post']);
+Route::get('/device', [App\Http\Controllers\AddDeviceController::class, 'list'])->middleware('auth')->name('device');
+Route::get('deletedevice/{id}', [App\Http\Controllers\UpdateController::class, 'delete']);
+
 
 
