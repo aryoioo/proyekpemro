bf27634c5f65d66e4bf654f54b29d28fd1c96b9c
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2024-06-30 15:32:49 +0700

    Update

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2024 at 08:01 PM
+-- Generation Time: Jun 30, 2024 at 10:30 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
